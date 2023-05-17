d5b98328e9633edbb86a61188aeee5f62baf871c
Margarida Pinto Borbinha <mpborbinha@corporativo.pt>
2023-05-17 17:01:29 +0100

    Add factory pattern to create strategies Add Country Enum Change cleaning.py Change and add tests

---
+++ life_expectancy/country.py
@@ -0,0 +1,72 @@
+"""Country"""
+from enum import Enum
+
+
+class Country(Enum):
+    """Country Enum"""
+
+    AL = "AL"
+    AM = "AM"
+    AT = "AT"
+    AZ = "AZ"
+    BE = "BE"
+    BG = "BG"
+    BY = "BY"
+    CH = "CH"
+    CY = "CY"
+    CZ = "CZ"
+    DE = "DE"
+    DE_TOT = "DE_TOT"
+    DK = "DK"
+    EA18 = "EA18"
+    EA19 = "EA19"
+    EE = "EE"
+    EEA30_2007 = "EEA30_2007"
+    EEA31 = "EEA31"
+    EFTA = "EFTA"
+    EL = "EL"
+    ES = "ES"
+    EU27_2007 = "EU27_2007"
+    EU27_2020 = "EU27_2020"
+    EU28 = "EU28"
+    FI = "FI"
+    FR = "FR"
+    FX = "FX"
+    GE = "GE"
+    HR = "HR"
+    HU = "HU"
+    IE = "IE"
+    IS = "IS"
+    IT = "IT"
+    LI = "LI"
+    LT = "LT"
+    LU = "LU"
+    LV = "LV"
+    MD = "MD"
+    ME = "ME"
+    MK = "MK"
+    MT = "MT"
+    NL = "NL"
+    NO = "NO"
+    PL = "PL"
+    PT = "PT"
+    RO = "RO"
+    RS = "RS"
+    RU = "RU"
+    SE = "SE"
+    SI = "SI"
+    SK = "SK"
+    SM = "SM"
+    TR = "TR"
+    UA = "UA"
+    UK = "UK"
+    XK = "XK"
+
+    @classmethod
+    def actual_countries(cls):
+        """Filters members of Country that aren't actually countries
+
+        Returns:
+            list: A list of actual countries
+        """
+        return [e[0] for e in vars(Country).items() if len(e[0]) == 2]
